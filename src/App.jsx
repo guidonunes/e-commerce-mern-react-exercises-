301780b4bb185207bbbd1fb6d847354ecd2fa2ad
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-17 16:50:52 -0300

    refactor code to exercise useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import NotFound from './pages/NotFound';
+import React, { useRef } from 'react';
 
 
 
+function App() {
+  let myHeadline = useRef()
 
-const App = () => {
+
+  const changeHeadline = () => {
+    myHeadline.innerHTML = "<ul><li>B<li>A</li></li></ul>";
+  }
 
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/about/:id/:name" element={<AboutPage/>} />
-          <Route path="/contact" element={<ContactPage/>} />
-          <Route path="*" element={< NotFound/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
+      <h1 ref={(h1)=>myHeadline=h1}></h1>
 
-export default App;
+      <button onClick={changeHeadline}>Click me!</button>
+    </div>
+  )
+}
+
+export default App
