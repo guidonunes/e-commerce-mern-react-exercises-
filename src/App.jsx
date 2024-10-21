870d89f49d68c3519f71a0a510b463953e68bccf
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-21 16:18:45 -0300

    implement async logic by using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
 
-function App() {
+import { useEffect, useState } from 'react';
 
-  let [formObj, setFormObj] = useState({
-    firstName: '',
-    lastName: '',
-    city: '',
-    gender: ''
-  });
+
+
+const App = () => {
+
+  let [data, setData] = useState();
+
+  useEffect(() => {
+    (async () => {
+    let response = await fetch('https://dummyjson.com/products/1')
+    let json = await response.json()
+    setData(json)
+    })
+  },[])
 
   return (
-    <div className="container">
-      <form action="">
-        <input value={formObj.firstName} placeholder='First Name' />
-        <input value={formObj.lastName} placeholder='Last Name' />
-        <select value={formObj.city}>
-          <option value="">Choose City</option>
-          <option value="Rio">Rio</option>
-          <option value="Salvador">Salvador</option>
-        </select>
-        <input  checked={formObj.gender === "Male"} type="radio" name="gender" />Male
-        <input checked={formObj.gender === "Female"} type="radio" name="gender" />Female
-        <br />
-        <button type='submit'>Submit</button>
-      </form>
+    <div>
+      {JSON.stringify(data)}
     </div>
   )
 }
 
-export default App;
+export default App
